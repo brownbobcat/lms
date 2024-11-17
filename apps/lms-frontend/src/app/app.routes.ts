056d21958224838dc3bb52160885b5b00282b862
Kojo-Brown <nicholasbrown988@gmail.com>
2024-11-17 04:43:30 -0500

    feat: create auth pages

--- apps/lms-frontend/src/app/app.routes.ts
+++ apps/lms-frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/sign-up/sign-up.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    // canActivate: [canActivateAuthPages],
+  },
+  {
+    path: 'sign-up',
+    component: SignupComponent,
+    // canActivate: [canActivateAuthPages],
+  },
+];
