a877a7238c8c195818699f4bda7a532f5054ab0c
Kojo-Brown <nicholasbrown988@gmail.com>
2025-05-24 04:11:15 -0400

    fix

--- apps/lms-backend/src/main.ts
+++ apps/lms-backend/src/main.ts
@@ -12,14 +12,14 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin:
-    //   process.env.NODE_ENV === 'production'
-    //     ? ['https://lms-tawny-seven.vercel.app']
-    //     : ['http://localhost:4200'],
-    origin: [
-      'http://localhost:4200', // Always allow local development
-      'https://lms-tawny-seven.vercel.app', // Always allow your Vercel app
-    ],
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? ['https://lms-five-navy.vercel.app/']
+        : ['http://localhost:4200'],
+    // origin: [
+    //   'http://localhost:4200', // Always allow local development
+    //   'https://lms-five-navy.vercel.app/', // Always allow your Vercel app
+    // ],
     credentials: true,
   });
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
